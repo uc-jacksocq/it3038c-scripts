e00b35b7a74263f9495ef6589930201482018b78
uc-jacksocq <77593056+uc-jacksocq@users.noreply.github.com>
2021-02-21 23:17:48 -0500

    Add files via upload

---
+++ car_game1.py
@@ -0,0 +1,18 @@
+# Chase Game
+#jacksocq
+# Part 1: Getting Started
+
+import snakegame
+import turtle
+import random
+
+#set up the screen
+wn = turtle.Screen()
+wn.title("Chase game")
+wn.bgcolor("blue")
+wn.bgcolor("red")
+wn.setup(width=680, height=680)
+wn.tracer(0)
+
+
+wn.mainloop()
